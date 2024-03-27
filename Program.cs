e0528997bd52a433ce72b599ac0b7dc762930458
cowqun <eminovcowqun96@gmail.com>
2024-03-27 16:06:24 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp59
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 80, 90, 100 };
+
+            double average = CalcAvg(grades); 
+
+           
+            if (average > 60)
+            {
+                Console.WriteLine("Mezun oldunuz");
+            }
+            else
+            {
+                Console.WriteLine("Mezun olamadınız");
+            }
+        }
+
+        static double CalcAvg(int[] grades)
+        {
+            int sum = 0;
+            foreach (int grade in grades)
+            {
+                sum += grade;
+            }
+            return (double)sum / grades.Length;
+        }
+    }
+}
+   
